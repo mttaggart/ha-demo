21cae8367e824ce1287f0cf80e71e26318fd5c6e
mttaggart <michael.t.taggart@gmail.com>
2019-06-07 07:59:24 -0700

    Finish demo

--- src/index.js
+++ src/index.js
@@ -5,11 +5,35 @@ const state = {
     message: "0"
 }
 
-const actions = {}
+const fizzBuzz = (n) => {
+    if (n % 15 == 0) {
+        return "FizzBuzz";
+    } else if (n % 5 == 0) {
+        return "Buzz";
+    } else if (n % 3 == 0) {
+        return "Fizz";
+    } else {
+        return n.toString();
+    }
+}
+
+const actions = {
+    increment: value => state => ({
+        count: state.count + value,
+        message: fizzBuzz(state.count + value)
+    }),
+    decrement: value => state => ({
+        count: state.count - value,
+        message: fizzBuzz(state.count - value)
+    }),
+}
 
 const view = (state, actions) => (
     <div>
-        
+        <h1>HyperFizz!</h1>
+        <h2>{state.count}: {state.message}</h2>
+        <button onclick={() => actions.increment(2)}>+</button>
+        <button onclick={() => actions.decrement(2)}>-</button>
     </div>
 )
 
